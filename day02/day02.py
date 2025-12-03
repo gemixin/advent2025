e18e8abcd032be292c7f37c472e99b5becd51459
Gemma McLean <gemixin@gmail.com>
2025-12-03 10:33:20 +0000

    Day 2 completed

---
+++ day02/day02.py
@@ -0,0 +1,75 @@
+"""
+Advent of Code 2025
+https://adventofcode.com/2025/day/2
+Gemma McLean
+"""
+
+# Lists for start and end ranges
+starts = []
+ends = []
+# Read in the input file and store in lists
+with open('day02/input.txt') as file_object:
+    # Get ranges separated by commas
+    ranges = file_object.readline().strip().split(',')
+    for r in ranges:
+        # Get start and end from each item
+        start, end = r.split('-')
+        # Append as integers to respective lists
+        starts.append(int(start))
+        ends.append(int(end))
+
+# ------
+# Part 1
+# ------
+
+# Count of invalid IDs
+invalid_ids = 0
+
+# Process each range
+for start, end in zip(starts, ends):
+    # Check each number in the range
+    for i in range(start, end + 1):
+        # Convert number to string for easier digit access
+        num_str = str(i)
+        # Check whether the length of the number is even
+        # If it's odd, we don't even need to check it as it can't be invalid
+        if len(num_str) % 2 == 0:
+            # Split num_str in half
+            mid = len(num_str) // 2
+            first_half = num_str[:mid]
+            second_half = num_str[mid:]
+            # Check if both halves are the same
+            if first_half == second_half:
+                # If they are, add number to invalid count
+                invalid_ids += i
+
+# Print the result
+print(f'Part 1: {invalid_ids}')
+
+# ------
+# Part 2
+# ------
+
+# Count of invalid IDs
+invalid_ids = 0
+
+# Process each range
+for start, end in zip(starts, ends):
+    # Check each number in the range
+    for i in range(start, end + 1):
+        # Convert number to string for easier digit access
+        num_str = str(i)
+        # Check each possible group size from half the length down to 1
+        for j in range(len(num_str)//2, 0, -1):
+            # If the length of the number is divisible by the group size
+            if len(num_str) % j == 0:
+                # Split num_str into groups of size j
+                groups = [num_str[k:k + j] for k in range(0, len(num_str), j)]
+                # Check if all groups are the same
+                if all(group == groups[0] for group in groups):
+                    # If they are, add number to invalid count and break
+                    invalid_ids += i
+                    break
+
+# Print the result
+print(f'Part 2: {invalid_ids}')
